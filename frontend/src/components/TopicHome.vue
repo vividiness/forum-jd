55ad2d416411d2bfc1224d7baae1acbed3c9da9f
Nelson Oliveira <antonionelsonsof@gmail.com>
2024-08-06 14:54:06 -0300

    [frontend] fixed nav page

--- frontend/src/components/TopicHome.vue
+++ frontend/src/components/TopicHome.vue
@@ -1,11 +1,17 @@
 <template>
     <div id="app">
+
+
       <header class="top-bar">
         <a class="prefeitura-logo" href="https://jd.fortaleza.ce.gov.br/" target="_blank">
             <img src="../assets/logo-pref.png" alt="Logo da Prefeitura"/>
+
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
         </a>
 
         <nav class="top-nav">
+
           <a href="#">Início</a>
           <a href="#">Sobre</a>
           <a href="#">Cursos</a>
@@ -21,11 +27,13 @@
       </header>
 
       <aside class="side-bar">
-        <a href="#">Página Inicial</a>
-        <a href="#">Popular</a>
-        <a href="#">Explorar</a>
-        <a href="#">Criar Feed Personalizado</a>
-        <a href="#">Favoritos</a>
+        
+
+        <a href="#"><i class="bi bi-house-door"></i> Página Inicial </a>
+        <a href="#"><i class="bi bi-arrow-up-right-circle"></i> Popular</a>
+        <a href="#"><i class="bi bi-zoom-in"></i> Explorar</a>
+        <a href="#"><i class="bi bi-layout-text-window"></i> Criar Feed Personalizado</a>
+        <a href="#"><i class="bi bi-star"></i> Favoritos</a>
       </aside>
 
 
